d44321e4266b2cd68bcffd90cff933e7ba8eb433
Hical <h41kal.pr@gmail.com>
2025-03-11 15:06:07 +0700

    request

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-from flask import Flask, request
-from flask_restful import Api, Resource
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video", required= True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required= True)
+video_put_args.add_argument("likes", type=int, help="likes on the video", required= True)
+
 videos = {}
+
+def abort_if_no_video_id(video_id):
+    if video_id not in videos:
+        abort(404, message="invalid video id")
 
 class Video(Resource):
     def get(self, video_id):
+        abort_if_no_video_id(video_id)
         return videos[video_id]
+    
     def put(self, video_id):
-        print(request.form)
-        return{}
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
     
     
 api.add_resource(Video, "/video/<int:video_id>")
